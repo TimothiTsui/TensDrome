93279345051bfcbebbe34cedd76c07ccb5f728a9
Tim Xu <ting.x@wustl.edu>
2023-04-27 23:26:54 -0500

    fix frequency
    Fixed the frequency, added time signature parameter but wasn't able to resolve the downbeat in a correct way.

--- main.py
+++ main.py
@@ -3,25 +3,67 @@ import time
 
 RelayPin = 18
 
+# Define Const
+simple_meters = [
+    "2/2",  # Cut time or Alla breve
+    "2/4",
+    "2/8",
+    "3/2",
+    "3/4",
+    "3/8",
+    "4/2",
+    "4/4",  # Common time
+    "4/8",
+]
+
+compound_meters = [
+    "6/4",
+    "6/8",
+    "9/4",
+    "9/8",
+    "12/4",
+    "12/8",
+]
+
+
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RelayPin, GPIO.OUT)
     GPIO.output(RelayPin, GPIO.LOW)
 
-def play_metronome(bpm):
+
+def play_metronome(bpm, time_signature):
     interval = 60.0 / bpm
+    beats_per_measure, beat_duration = map(int, time_signature.split('/'))
+    beats = beats_per_measure * (beat_duration // 4)
+
+    count = 1
     while True:
-        print("Tick")
+        if time_signature in simple_meters:
+            if count % beats == 1:
+                print("Downbeat")
+            else:
+                print(count % beats or beats)
+        else:
+
+            #Compound meters still doesn't print out correctly
+            if count % beats == 1 or count % (beats // 2) == 1:
+                print("Downbeat")
+            else:
+                print(count % beats or beats)
+
         GPIO.output(RelayPin, GPIO.HIGH)
         time.sleep(interval / 2)
         GPIO.output(RelayPin, GPIO.LOW)
-
-        print("Tock")
         time.sleep(interval / 2)
+
+        count += 1
+
 
 def destroy():
     GPIO.output(RelayPin, GPIO.LOW)
     GPIO.cleanup()
+
 
 if __name__ == "__main__":
     setup()
@@ -29,7 +71,12 @@ if __name__ == "__main__":
         bpm = int(input("Enter the BPM (Beats Per Minute): "))
         if bpm < 1:
             raise ValueError("BPM must be a positive integer.")
-        play_metronome(bpm)
+
+        time_signature = input("Enter the time signature (e.g., 4/4, 6/8, 3/4): ")
+        if not time_signature or len(time_signature.split('/')) != 2:
+            raise ValueError("Invalid time signature format.")
+
+        play_metronome(bpm, time_signature)
     except ValueError as ve:
         print(ve)
     except KeyboardInterrupt:
