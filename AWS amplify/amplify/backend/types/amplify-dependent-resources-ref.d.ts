35cde36edae5437edf743a8511d9c3ff7f9fec32
Tim Xu <ting.x@wustl.edu>
2023-05-12 21:56:30 -0500

    Auto stash before rebase of "refs/heads/Frontend"

--- AWS amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "tenscontrolpanel": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
