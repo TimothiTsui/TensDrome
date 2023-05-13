9af0e6e3d02ce51160a10f3a8d76de82b6075be4
Tim Xu <ting.x@wustl.edu>
2023-05-12 21:30:33 -0500

    FrontPanel-Amplify

---
+++ AWS amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tenscontrolpanel": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
